26e4feceb617d4af37b2b8dbfc42e0f7b3a5d3a0
Roberta Giambona <rgiambon@car12s2.42barcelona.com>
2024-10-28 13:37:42 +0100

    add: lexer reading

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: rgiambon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:35:08 by rgiambon          #+#    #+#             */
-/*   Updated: 2024/10/28 11:59:21 by rgiambon         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:37:23 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,61 @@ int	is_whitespace(char c)
 	return (0);
 }
 
+int	store_right_brackets(char *input, t_lexer **token_list)
+{
+	if (*(input + 1) == '<')
+	{
+		create_new_node_token("<<", APPEND, token_list);
+		//add node to the list
+		return (2);
+	}
+	create_new_node_token("<", REDIR_IN, token_list);
+	return (1);
+}
+
+int	store_left_brackets(char *input, t_lexer **token_list)
+{
+	if (*(input + 1) == '<')
+	{
+		create_new_node_token("<<", APPEND, token_list);
+		//add node to the list
+		return (2);
+	}
+	create_new_node_token("<", REDIR_IN, token_list);
+	return (1);
+}
+
+int	store_word(char *input, t_lexer **token_list)
+{
+	int	i;
+
+	i = 0;
+	while (input[i])
+	{
+		//add some quotes handling
+		if (input[i] == '<' || input[i] == '>' || input [i] == '|')
+			break ;
+		i++
+	}
+	create_new_node_token(ft_substr(input, 0, i), WORD, token_list);
+	//add node to list
+	return (i);
+}
+
 int	store_token(char *input, t_lexer **token_list)
 {
+	if (*input == '<')
+		return (store_left_brackets(input, token_list));
+	else if (*input == '>')
+		return (store_right_brackets(input, token_list));
+	else if (*input == '|')
+	{
+		create_new_node_token("|", PIPE, token_list);
+		//add node to list
+	}
+	else
+		return (store_word(input, token_list);
+	return (1);
 }
 
 t_lexer	read_token(char *input)
@@ -32,7 +85,7 @@ t_lexer	read_token(char *input)
 
 	token_list = NULL;
 	i = 0;
-	//remove leading and trailing white spaces
+	//remove leading and trailing white spaces:
 	input = ft_strtrim(input, " ");
 	while (input[i])
 	{
@@ -40,10 +93,4 @@ t_lexer	read_token(char *input)
 			i++;
 		i = i + store_token(&input[i], &token_list);
 	}
-}
-		
-		
-		
-	
-
 }
