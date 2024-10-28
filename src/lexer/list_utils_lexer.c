565dd1fb2533334e4346964215e10caa6e7c72b0
Roberta Giambona <rgiambon@car12s2.42barcelona.com>
2024-10-28 14:54:23 +0100

    add node

--- src/lexer/list_utils_lexer.c
+++ src/lexer/list_utils_lexer.c
@@ -6,11 +6,27 @@
 /*   By: rgiambon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:51:06 by rgiambon          #+#    #+#             */
-/*   Updated: 2024/10/28 14:17:49 by rgiambon         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:54:05 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+void	add_node_lexer(t_list *new_node, t_lexer **token_list)
+{
+	t_lexer	*temp_node;
+
+	temp_node = *token_list;
+	if (*token_list == NULL)
+	{
+		*token_list = new_node;
+		return ;
+	}
+	while (temp_node->next != NULL)
+		temp_node = temp_node->next;
+	temp_node->next = new_node;
+	new_node->prev = temp_node;
+}
 
 t_lexer	*new_node_lexer(char *str, int type, t_lexer **token_list)
 {
