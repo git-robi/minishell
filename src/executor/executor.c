b60161603a50792a30a5c7a68621f7de0b341c1d
Roberta Giambona <nemo@Robertas-MacBook-Pro.local>
2024-11-15 12:24:47 +0100

    wip: executor

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,3 +1,15 @@
+
+void	multiple_commands(t_mini *data)
+{
+	//call the expander;
+
+	//set up pipes
+
+	//handle heredoc
+
+	//AND THEN????
+}
+
 void	executor(t_mini *data)
 {
 	//if there are zero pipes:
@@ -6,5 +18,5 @@ void	executor(t_mini *data)
 	//else
 		//create array to store the process IDs of the child processes
 		//call function that executes all commands with pipes
-
+}
 	
