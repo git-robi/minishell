bfa784e442daddd9f4420bec56f54b760ebe3f5b
Roberta Giambona <nemo@Robertas-MacBook-Pro.local>
2024-11-15 12:03:53 +0100

    wip: add executor and expander folders

---
+++ executor/executor.c
@@ -0,0 +1,10 @@
+void	executor(t_mini *data)
+{
+	//if there are zero pipes:
+		//call function that executes a single command
+	
+	//else
+		//create array to store the process IDs of the child processes
+		//call function that executes all commands with pipes
+
+	
