44152a4f1b613955bb129f8ad1749e9788f460a4
robi <giambonaroberta0@gmail.com>
2024-12-12 20:10:15 +0100

    wip: fix norm includes

--- includes/lexer.h
+++ includes/lexer.h
@@ -11,47 +11,40 @@
 /* ************************************************************************** */
 
 #ifndef LEXER_H
-#define LEXER_H
+# define LEXER_H
 
-//#include "mini.h"
+# define WORD		1
+# define PIPE		2
+# define REDIR_IN	3
+# define REDIR_OUT	4
+# define APPEND		5
+# define HERE_DOC	6
 
-#define	WORD		1
-#define	PIPE		2
-#define	REDIR_IN	3
-#define	REDIR_OUT	4
-#define	APPEND		5
-#define	HERE_DOC	6
+typedef struct	s_lexer t_lexer;
+typedef struct	s_mini t_mini;
 
-typedef struct s_lexer t_lexer;
-typedef struct s_mini t_mini;
-
-typedef struct s_lexer
+typedef struct	s_lexer
 {
 	char	*token;
-	int		type;
+	int	type;
 	t_lexer	*next;
 	t_lexer	*prev;
 }	t_lexer;
 
 void	read_token(t_mini *data);
-int 	is_whitespace(char c);
-
-//store tokens
-int 	store_token(char *input, t_mini *data);
+int	is_whitespace(char c);
+int	store_token(char *input, t_mini *data);
 int	store_word(char *input, t_lexer **token_list);
-int 	store_left_brackets(char *input, t_lexer **token_list);
-int 	store_right_brackets(char *input, t_lexer **token_list);
-
-//list utils lexer
-t_lexer *new_node_lexer(char *str, int type);
-void    add_node_lexer(t_lexer *new_node, t_lexer **token_list);
-void    delete_node_lexer(t_mini *data, t_lexer **node);
-void    free_node_lexer(t_lexer **node);
-t_lexer *delete_redirection_lexer(t_mini *data, t_lexer **node);
-//quotes
-int    count_quotes(char *line);
-int     store_token_in_quotes(char *input, t_lexer **token_list);
-int is_quote(char c);
-int     is_double_quoted(char *str);
+int	store_left_brackets(char *input, t_lexer **token_list);
+int	store_right_brackets(char *input, t_lexer **token_list);
+t_lexer	*new_node_lexer(char *str, int type);
+void	add_node_lexer(t_lexer *new_node, t_lexer **token_list);
+void	delete_node_lexer(t_mini *data, t_lexer **node);
+void	free_node_lexer(t_lexer **node);
+t_lexer	*delete_redirection_lexer(t_mini *data, t_lexer **node);
+int	count_quotes(char *line);
+int	store_token_in_quotes(char *input, t_lexer **token_list);
+int	is_quote(char c);
+int	is_double_quoted(char *str);
 int	is_single_quoted(char *str);
 #endif
--- includes/parser.h
+++ includes/parser.h
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 
 #ifndef PARSER_H
-#define PARSER_H
+# define PARSER_H
 
-typedef struct s_parser t_parser;
+typedef struct	s_parser t_parser;
 
-typedef struct s_parser
+typedef struct	s_parser
 {
-	char	**commands;
-	int	builtin;
+	char		**commands;
+	int		builtin;
 	char		*heredoc_name;
 	char		*heredoc_delim;
 	t_lexer		*redirections;
@@ -27,21 +27,17 @@ typedef struct s_parser
 }	t_parser;
 
 void		parser(t_mini *data);
-void	store_commands(t_mini *data, t_parser **node);
-t_lexer *remove_pipe(t_mini *data);
-void    handle_redirections(t_mini *data, t_parser *node);
-t_lexer    *store_redirection(t_lexer **token, t_parser **pars_node, t_mini *data);
-
-//list utils parser
-void    add_node_parser(t_parser *node, t_parser **parser_list);
-t_parser        *new_node_parser(char *cmd);
-void    free_node_parser(t_parser **node);
-int     count_nodes(t_parser *parser_list);
-
-//utils parser
-int             count_commands(t_mini *data);
-t_lexer             *error_check(t_mini *data);
-int    unexpected_token_error(t_lexer *node);
-int     check_builtin(char *cmd);
-t_lexer *get_lexer_head(t_lexer *node);
+void		store_commands(t_mini *data, t_parser **node);
+t_lexer		*remove_pipe(t_mini *data);
+void		handle_redirections(t_mini *data, t_parser *node);
+t_lexer		*store_redirection(t_lexer **token, t_parser **pars_node, t_mini *data);
+void		add_node_parser(t_parser *node, t_parser **parser_list);
+t_parser	*new_node_parser(char *cmd);
+void		free_node_parser(t_parser **node);
+int		count_nodes(t_parser *parser_list);
+int		count_commands(t_mini *data);
+t_lexer		*error_check(t_mini *data);
+int		unexpected_token_error(t_lexer *node);
+int		check_builtin(char *cmd);
+t_lexer		*get_lexer_head(t_lexer *node);
 #endif
