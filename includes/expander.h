477ff3f9b9968e39afd5afb133e840dfc771aad9
Roberta Giambona <rgiambon@cbr5s1.42barcelona.com>
2024-12-18 14:06:45 +0100

    fix: norm expander.h

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: rgiambon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:46:33 by rgiambon          #+#    #+#             */
-/*   Updated: 2024/12/16 11:14:33 by rgiambon         ###   ########.fr       */
+/*   Updated: 2024/12/18 14:06:23 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 
 typedef struct s_quotes
 {
-        int     f_s;
-        int     f_d;
+	int	f_s;
+	int	f_d;
 	int	s_s;
 	int	s_d;
-}       t_quotes;
+}	t_quotes;
 
 typedef struct s_xy
 {
@@ -29,7 +29,7 @@ typedef struct s_xy
 	int	end;
 }	t_xy;
 
-int	expander(t_mini *data);
+int		expander(t_mini *data);
 void	expand_string(t_mini *data, char **string);
 char	*handle_question_mark(t_mini *data, char *string, \
 int start, int exit_code);
@@ -42,12 +42,12 @@ char	*expand_and_reassemble(t_mini *data, char *string, t_xy xy, \
 char *expansion);
 char	*env_expand(t_mini *data, char **tmp, char **string);
 void	clean_quotes(t_mini *data);
-void    clean_redirections(t_mini *data, t_parser *tmp, int *marker_count);
+void	clean_redirections(t_mini *data, t_parser *tmp, int *marker_count);
 char	*remove_marker(t_mini *data, char *str, int marker_count, char marker);
 void	replace_quotes(char *str, int *marker_count, int i);
-int	expand_redirections(t_mini *data, t_parser *node);
-int	space_in_redirection(char *redir);
-int	count_marker(char *str, char marker);
+int		expand_redirections(t_mini *data, t_parser *node);
+int		space_in_redirection(char *redir);
+int		count_marker(char *str, char marker);
 void	clean_cmd(char *cmd);
 void	clean_spaces(t_mini *data);
 void	clean_markers(t_mini *data);
